381667712e738469d926aa4b4a07a780ab709c0c
Jacob Holtom <jacob@holtom.me>
2020-06-18 12:47:55 -0700

    Updated to first test version

--- launch_wiscanet_x310.sh
+++ launch_wiscanet_x310.sh
@@ -8,8 +8,8 @@ mkdir -p ${HOME}/wdemo
 sudo podman volume create --opt type=none --opt o=bind --opt device=${HOME}/wdemo cnode_wdemo
 
 # Specify Radio Device Strings eg. addr=1.2.3.4 here
-RADIO0="addr="
-RADIO1="addr="
+RADIO0="serial=30F411A"
+RADIO1="serial=30F419C"
 
 # Specify Node MAC Addresses for MATLAB
 CNODE_MAC_ADDR="A8:5E:45:8E:90:53"
@@ -18,19 +18,17 @@ ENODE1_MAC_ADDR="A8:5E:45:2A:6F:91"
 
 echo "Launching ENODE0 for Radio 0..."
 echo "Radio 0 located at ${RADIO0}"
-ENODE0_ID=$(sudo podman run --rm -dt --cap-add=sys_nice --mac-address="${ENODE0_MAC_ADDR}" --name enode0 localhost/wisca/wiscanet)
+ENODE0_ID=$(sudo podman run --rm -dt --privileged --cap-add=sys_nice --mac-address="${ENODE0_MAC_ADDR}" --name enode0 localhost/wisca/wiscanet)
 echo "Launched with ID: ${ENODE0_ID}"
 ENODE0_IP=$(sudo podman inspect -f "{{.NetworkSettings.IPAddress}}" enode0)
 echo "IP Address of ENODE0: ${ENODE0_IP}"
 
-
 echo "Launching ENODE1 for Radio 1..."
 echo "Radio 1 located at ${RADIO1}"
-ENODE1_ID=$(sudo podman run --rm -dt --cap-add=sys_nice --mac-address="${ENODE1_MAC_ADDR}"   --name enode1 localhost/wisca/wiscanet)
+ENODE1_ID=$(sudo podman run --rm -dt --privileged --cap-add=sys_nice --mac-address="${ENODE1_MAC_ADDR}"   --name enode1 localhost/wisca/wiscanet)
 echo "Launched with ID: ${ENODE1_ID}"
 ENODE1_IP=$(sudo podman inspect -f "{{.NetworkSettings.IPAddress}}" enode1)
 echo "IP Address of ENODE1: ${ENODE1_IP}"
-
 
 echo "Launching CNODE..."
 CNODE_ID=$(sudo podman run --rm -dt --privileged --mac-address="${CNODE_MAC_ADDR}" -v cnode_wdemo:/home/wisca/wdemo --name cnode localhost/wisca/wiscanet)
@@ -44,6 +42,8 @@ sudo podman exec cnode /bin/bash -c "echo ${ENODE0_IP} > /home/wisca/wdemo/run/u
 sudo podman exec cnode /bin/bash -c "echo ${ENODE1_IP} >> /home/wisca/wdemo/run/usr/cfg/iplist"
 sudo podman exec cnode /bin/bash -c "mv /home/wisca/wdemo/run/usr/cfg/usrconfig_node0.xml /home/wisca/wdemo/run/usr/cfg/usrconfig_${ENODE0_IP}.xml"
 sudo podman exec cnode /bin/bash -c "mv /home/wisca/wdemo/run/usr/cfg/usrconfig_node1.xml /home/wisca/wdemo/run/usr/cfg/usrconfig_${ENODE1_IP}.xml"
+sudo podman exec cnode /bin/bash -c "sed -i 's/replace_me/${RADIO0}/' /home/wisca/wdemo/run/usr/cfg/usrconfig_node0.xml"
+sudo podman exec cnode /bin/bash -c "sed -i 's/replace_me/${RADIO1}/' /home/wisca/wdemo/run/usr/cfg/usrconfig_node1.xml"
 
 # Handle SSH configuration
 # Generate keys on each node
